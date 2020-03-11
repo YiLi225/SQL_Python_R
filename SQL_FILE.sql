2eac82956d83516517194a1a157e0123bb3f5d38
YiLi225 <yilistats@gmail.com>
2020-03-10 21:24:12 -0700

    rewrite the notes

--- SQL_FILE.sql
+++ SQL_FILE.sql
@@ -1,9 +1,6 @@
 
 --- 6) Read in SQL queries without copying and pasting 
---- We want to extract only records for ID '19228' and '19272' into R and Python, and we don't want to copy and paste the 
---- original SQL code because otherwise, it would be difficult for code update and maintainance. Here for demonstration, 
---- let's connect through ODBC
--- The trick here is to 
+--- We want to extract only records for ID '19228' and '19272'
 SELECT 
   *
 FROM 
